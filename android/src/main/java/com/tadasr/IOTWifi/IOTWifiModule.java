17b3f3e2ae4bec8433ec41fd83c02ed38ed7fa1e
Tadas Razmislavicius <tadas.razmislavicius@gmail.com>
2018-01-14 20:48:32 +0200

    Updating java packague name

--- android/src/main/java/com/tadasr/IOTWifi/IOTWifiModule.java
+++ android/src/main/java/com/tadasr/IOTWifi/IOTWifiModule.java
@@ -1,4 +1,4 @@
-package com.nectarsun.IOTWifi;
+package com.tadasr.IOTWifi;
 
 import com.facebook.react.bridge.*;
 import android.net.wifi.WifiInfo;
--- android/src/main/java/com/tadasr/IOTWifi/IOTWifiPackage.java
+++ android/src/main/java/com/tadasr/IOTWifi/IOTWifiPackage.java
@@ -1,4 +1,4 @@
-package com.nectarsun.IOTWifi;
+package com.tadasr.IOTWifi;
 
 import java.util.Arrays;
 import java.util.Collections;
