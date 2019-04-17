df6ab2ce06a2290ae19d7641db7a246594fed06c
Edward Paget <ed.paget@gmail.com>
2019-04-17 08:50:32 -0400

    Add seperate connect args

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,9 @@
 declare module 'react-native-iot-wifi' {
   // tslint:disable:no-namespace
+  type ConnectArgs =
+    | [string, (error: string) => void]
+    | [string, boolean, (error: string) => void];
+
   type RemoveConnectArgs =
     | [string, (error: string) => void]
     | [string, boolean, (error: string) => void];
@@ -11,7 +15,7 @@ declare module 'react-native-iot-wifi' {
   export namespace RNWifi {
     function isAvailable(cb: (available: boolean) => void): void;
     function getSSID(cb: (ssid: string) => void): void;
-    function connect(...args: RemoveConnectArgs): void;
+    function connect(...args: ConnectArgs): void;
     function connectSecure(...args: ConnectSecureArgs): void;
     function removeSSID(...args: RemoveConnectArgs): void;
   }
